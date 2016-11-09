26bec2ae53c1cbb2293492949bd8d0a6f71e502d
Daniel Semac <dsemac@gmail.com>
2016-11-08 20:40:57 -0600

    Added FireAngle script, stubbed for now

---
+++ Assets/Scripts/ShooterGame/Player/FireAngle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireAngle : MonoBehaviour {
+
+    public float Angle
+    {
+        get { return 1.0f; }
+        private set {}
+    }
+
+    void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
